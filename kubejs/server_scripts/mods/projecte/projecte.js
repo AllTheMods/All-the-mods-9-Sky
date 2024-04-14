c28807c6f5f93288c226ffe02ca88666aefc718d
oly2o6 <calumroberts02@hotmail.com>
2024-04-14 07:10:10 +0100

    projecte fixes

---
+++ kubejs/server_scripts/mods/projecte/projecte.js
@@ -0,0 +1,8 @@
+ServerEvents.recipes(e =>{
+  e.remove({id: 'projecte:philosophers_stone_alt'})
+  e.remove({id: 'projecte:philosophers_stone'})
+  e.shaped('projecte:philosophers_stone', ['GBG', 'BIB', 'GBG'], {
+    I: 'minecraft:nether_star',
+    G: '#forge:gunpowder',
+    B: 'minecraft:blaze_powder'
+  })
