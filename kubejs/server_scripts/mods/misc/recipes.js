f11f3062606af877fd0551af66ba55dc163da624
billbertking1 <billbertking1@users.noreply.github.com>
2024-04-18 17:39:23 -0500

    beta 4 update

---
+++ kubejs/server_scripts/mods/misc/recipes.js
@@ -0,0 +1,12 @@
+ServerEvents.recipes( event => {
+
+// Time in a bottle
+	event.remove({ id: 'tiab:time_in_a_bottle' })
+ 	event.shaped('tiab:time_in_a_bottle', ['UDU', 'LCL', 'LBL'], {
+ 		U: 'mysticalagriculture:awakened_supremium_block',
+    	D: 'productivebees:upgrade_productivity_4',
+    	L: 'allthecompressed:diamond_block_2x',
+    	C: 'projecte:watch_of_flowing_time',
+    	V: 'allthecompressed:nether_star_block_3x'
+  })
+})
