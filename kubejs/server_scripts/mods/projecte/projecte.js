157bf1bf21d1c45427ed8ec91c97130fac641b31
oly2o6 <calumroberts02@hotmail.com>
2024-04-27 00:31:32 +0100

    The dreaded projecte changes

--- kubejs/server_scripts/mods/projecte/projecte.js
+++ kubejs/server_scripts/mods/projecte/projecte.js
@@ -1,9 +1,38 @@
 ServerEvents.recipes(e =>{
+
+
+  //darkmatter
+  e.remove({id: 'projecte:dark_matter'})
+  e.recipes.summoningrituals
+  .altar('projecte:aeternalis_fuel_block')
+  .input('allthemodium:allthemodium_ingot')
+  .input('minecraft:netherite_scrap')
+  .input('megacells:sky_steel_ingot')
+  .input('alltheores:enderium_ingot')
+  .itemOutput('projecte:dark_matter')
+  .recipeTime(10)
+
   e.remove({id: 'projecte:philosophers_stone_alt'})
   e.remove({id: 'projecte:philosophers_stone'})
-  e.shaped('projecte:philosophers_stone', ['GBG', 'BIB', 'GBG'], {
-    I: 'allthetweaks:nether_star_block',
-    G: 'thermal:gunpowder_block',
-    B: 'allthecompressed:blaze_block_1x'
-    })
-  })
+  e.recipes.summoningrituals
+  .altar('minecraft:tnt')
+  .input('forbidden_arcanus:dark_nether_star')
+  .input('2x botania:manasteel_ingot')
+  .input('2x minecraft:blaze_rod')
+  .input('minecraft:dragon_breath')
+  .itemOutput('projecte:philosophers_stone')
+  .recipeTime(20)
+
+  //redmatter
+  e.remove({id: 'projecte:red_matter_alt'})
+  e.remove({id: 'projecte:red_matter'})
+  e.recipes.summoningrituals
+  .altar('projecte:dark_matter')
+  .input('allthemodium:unobtainium_ingot')
+  .input('2x mysticalagriculture:supremium_essence')
+  .input('enderio:prescient_crystal')
+  .input('minecraft:phantom_membrane')
+  .itemOutput('projecte:red_matter')
+  .recipeTime(20)  
+
+})
