c087d7a265dc935ff205d002329f393938acd748
oly2o6 <calumroberts02@hotmail.com>
2024-05-03 01:03:15 +0100

    Time in a bottle recipe+remove teleport pad

--- kubejs/server_scripts/mods/misc/recipes.js
+++ kubejs/server_scripts/mods/misc/recipes.js
@@ -1,12 +1,14 @@
 ServerEvents.recipes( event => {
 
 // Time in a bottle
-	/*event.remove({ id: 'tiab:time_in_a_bottle' })
- 	event.shaped('tiab:time_in_a_bottle', ['UDU', 'LCL', 'LBL'], {
- 		U: 'mysticalagriculture:awakened_supremium_block',
-    	D: 'productivebees:upgrade_productivity_4',
-    	L: 'allthecompressed:diamond_block_2x',
-    	C: 'projecte:watch_of_flowing_time',
-    	B: 'allthecompressed:nether_star_block_3x'
-  }) */
+	event.remove({ id: 'tiab:time_in_a_bottle' })
+ 	event.shaped('tiab:time_in_a_bottle', ['NBN', 'QCQ', 'NNN'], {
+ 		N: 'minecraft:netherite_ingot',
+    	Q: 'minecraft:quartz',
+    	C: 'minecraft:clock',
+    	B: 'minecraft:glass_bottle'
+  }) 
+
+  //Teleport pad
+  event.remove({id: 'allthemodium:teleport_pad'})
 })
