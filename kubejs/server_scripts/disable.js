c7560b91629d1594f2e03023ffab33ec26a0614f
oly2o6 <calumroberts02@hotmail.com>
2024-06-04 22:54:52 +0100

    disable chunkloaders

---
+++ kubejs/server_scripts/disable.js
@@ -0,0 +1,5 @@
+ServerEvents.recipes(event => {
+    event.remove({ id:'ae2:network/blocks/spatial_anchor' })
+    event.remove({ id:'mekanism:dimensional_stabilizer'})
+    event.remove({ mod:'buildinggadgets2'})
+})
