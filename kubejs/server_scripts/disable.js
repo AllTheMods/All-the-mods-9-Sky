99df76e5378bc244802c4f8928f52985712ee777
oly2o6 <calumroberts02@hotmail.com>
2024-06-08 00:17:56 +0100

    Re-enable building gadgets

--- kubejs/server_scripts/disable.js
+++ kubejs/server_scripts/disable.js
@@ -1,5 +1,4 @@
 ServerEvents.recipes(event => {
     event.remove({ id:'ae2:network/blocks/spatial_anchor' })
     event.remove({ id:'mekanism:dimensional_stabilizer'})
-    event.remove({ mod:'buildinggadgets2'})
 })
