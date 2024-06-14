516871cbe165bbc2337730ba4567da5789d460e4
oly2o6 <calumroberts02@hotmail.com>
2024-06-14 01:48:49 +0100

    Disable the cheap smithing template recipe

--- kubejs/server_scripts/disable.js
+++ kubejs/server_scripts/disable.js
@@ -1,4 +1,5 @@
 ServerEvents.recipes(event => {
     event.remove({ id:'ae2:network/blocks/spatial_anchor' })
     event.remove({ id:'mekanism:dimensional_stabilizer'})
+    event.remove({ id:'danksmithing:netherite_upgrade_smithing_template'})
 })
