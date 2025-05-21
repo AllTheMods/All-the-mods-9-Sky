28cdcdf38434630751ec8b84198f3d51ab9638c9
Satherov <Satherov@users.noreply.github.com>
2025-05-22 00:44:55 +0200

    fix: remove netherite tempalte recipe

--- kubejs/server_scripts/mods/minecraft/recipes.js
+++ kubejs/server_scripts/mods/minecraft/recipes.js
@@ -1,20 +1,19 @@
+// This File has been authored by AllTheMods Staff, or a Community contributor for use in AllTheMods - AllTheMods 9: To the Sky.
+// As all AllTheMods packs are licensed under All Rights Reserved, this file is not allowed to be used in any public packs not released by the AllTheMods Team, without explicit permission.
+
 ServerEvents.recipes(event =>{
-  event.shaped('minecraft:elytra', ["aba", "cdc", "c c"], {
-    a: "minecraft:diamond",
-    b: "minecraft:string",
-    c: "minecraft:phantom_membrane",
-    d: "minecraft:nether_star"
-  })
-
-
-        //netherite template
-       event.shaped('minecraft:netherite_upgrade_smithing_template', [
-        'TCT',
-        'TLT',
-        'TTT'
-      ], {
-        T: 'minecraft:nether_brick',
-        L: 'minecraft:ender_eye',
-        C: 'minecraft:nether_star'
-      })
+    event.shaped('minecraft:elytra',
+        [
+            "aba",
+            "cdc",
+            "c c"
+        ], {
+        a: "minecraft:diamond",
+        b: "minecraft:string",
+        c: "minecraft:phantom_membrane",
+        d: "minecraft:nether_star"
+    })
 })
+
+// This File has been authored by AllTheMods Staff, or a Community contributor for use in AllTheMods - AllTheMods 9: To the Sky.
+// As all AllTheMods packs are licensed under All Rights Reserved, this file is not allowed to be used in any public packs not released by the AllTheMods Team, without explicit permission.
