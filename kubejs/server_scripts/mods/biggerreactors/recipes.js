b045a1885f68307442b44876a607d7fb9e7d1a22
Satherov <Satherov@users.noreply.github.com>
2025-05-22 22:15:46 +0200

    feat: remove duplicate uranium recipes by bigger reactors - resolves #692

---
+++ kubejs/server_scripts/mods/biggerreactors/recipes.js
@@ -0,0 +1,12 @@
+// This File has been authored by AllTheMods Staff, or a Community contributor for use in AllTheMods - AllTheMods 9: To the Sky.
+// As all AllTheMods packs are licensed under All Rights Reserved, this file is not allowed to be used in any public packs not released by the AllTheMods Team, without explicit permission.
+
+ServerEvents.recipes(allthemods => {
+    allthemods.remove({ id: 'biggerreactors:compat/mekanism/enriching/enrichment_deepslate_uranium_ore' })
+    allthemods.remove({ id: 'biggerreactors:compat/mekanism/enriching/enrichment_uranium_chunk' })
+    allthemods.remove({ id: 'biggerreactors:compat/mekanism/enriching/enrichment_uranium_ore' })
+    allthemods.remove({ id: 'biggerreactors:compat/mekanism/crushing/crusher_uranium_ingot' })
+})
+
+// This File has been authored by AllTheMods Staff, or a Community contributor for use in AllTheMods - AllTheMods 9: To the Sky.
+// As all AllTheMods packs are licensed under All Rights Reserved, this file is not allowed to be used in any public packs not released by the AllTheMods Team, without explicit permission.
