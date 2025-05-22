004b6fc8ff39f70d2e2827b4bc551e93ea5caea8
Satherov <Satherov@users.noreply.github.com>
2025-05-22 22:10:05 +0200

    feat: remove strange FS drawer recipes - resolves #696

---
+++ kubejs/server_scripts/mods/functionalstorage/recipes.js
@@ -0,0 +1,11 @@
+// This File has been authored by AllTheMods Staff, or a Community contributor for use in AllTheMods - AllTheMods 9: To the Sky.
+// As all AllTheMods packs are licensed under All Rights Reserved, this file is not allowed to be used in any public packs not released by the AllTheMods Team, without explicit permission.
+
+ServerEvents.recipes(allthemods => {
+    allthemods.remove({id: 'functionalstorage:oak_drawer_alternate_x1'})
+    allthemods.remove({id: 'functionalstorage:oak_drawer_alternate_x2'})
+    allthemods.remove({id: 'functionalstorage:oak_drawer_alternate_x4'})
+})
+
+// This File has been authored by AllTheMods Staff, or a Community contributor for use in AllTheMods - AllTheMods 9: To the Sky.
+// As all AllTheMods packs are licensed under All Rights Reserved, this file is not allowed to be used in any public packs not released by the AllTheMods Team, without explicit permission.
