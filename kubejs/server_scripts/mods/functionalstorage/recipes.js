361a1bc66ea3bedb6d1ee077364dd429698b4a97
Satherov <Satherov@users.noreply.github.com>
2025-06-21 12:46:16 +0200

    feat: custom compact crafting - resolves #249

--- kubejs/server_scripts/mods/functionalstorage/recipes.js
+++ kubejs/server_scripts/mods/functionalstorage/recipes.js
@@ -5,6 +5,44 @@ ServerEvents.recipes(allthemods => {
     allthemods.remove({id: 'functionalstorage:oak_drawer_alternate_x1'})
     allthemods.remove({id: 'functionalstorage:oak_drawer_alternate_x2'})
     allthemods.remove({id: 'functionalstorage:oak_drawer_alternate_x4'})
+
+    /**
+     * @param {ItemStack} lower
+     * @param {ItemStack} higher
+     */
+    function compact(lower, higher) {
+        let recipe = {
+            "type": "functionalstorage:custom_compacting",
+            "higher_input": higher.toJson(),
+            "lower_input": lower.toJson(),
+        }
+
+        allthemods.custom(recipe).id(`allthemods:functionalstorage/compacting/${lower.id.split(':')[1]}_to_${higher.id.split(':')[1]}`)
+    }
+
+    compact(Item.of('exdeorum:stone_pebble', 4), Item.of('minecraft:cobblestone'))
+    compact(Item.of('exdeorum:diorite_pebble', 4), Item.of('minecraft:diorite'))
+    compact(Item.of('exdeorum:andesite_pebble', 4), Item.of('minecraft:andesite'))
+    compact(Item.of('exdeorum:granite_pebble', 4), Item.of('minecraft:granite'))
+    compact(Item.of('exdeorum:deepslate_pebble', 4), Item.of('minecraft:deepslate'))
+    compact(Item.of('exdeorum:tuff_pebble', 4), Item.of('minecraft:tuff'))
+    compact(Item.of('exdeorum:calcite_pebble', 4), Item.of('minecraft:calcite'))
+    compact(Item.of('exdeorum:basalt_pebble', 4), Item.of('minecraft:basalt'))
+    compact(Item.of('exdeorum:blackstone_pebble', 4), Item.of('minecraft:blackstone'))
+
+    compact(Item.of('exdeorum:iron_ore_chunk', 4), Item.of('minecraft:iron_ore'))
+    compact(Item.of('exdeorum:copper_ore_chunk', 4), Item.of('minecraft:copper_ore'))
+    compact(Item.of('exdeorum:gold_ore_chunk', 4), Item.of('minecraft:gold_ore'))
+    compact(Item.of('exdeorum:aluminum_ore_chunk', 4), Item.of('alltheores:aluminum_ore'))
+    compact(Item.of('exdeorum:cobalt_ore_chunk', 4), Item.of('tconstruct:cobalt_ore'))
+    compact(Item.of('exdeorum:lead_ore_chunk', 4), Item.of('alltheores:lead_ore'))
+    compact(Item.of('exdeorum:silver_ore_chunk', 4), Item.of('alltheores:silver_ore'))
+    compact(Item.of('exdeorum:nickel_ore_chunk', 4), Item.of('alltheores:nickel_ore'))
+    compact(Item.of('exdeorum:tin_ore_chunk', 4), Item.of('alltheores:tin_ore'))
+    compact(Item.of('exdeorum:uranium_ore_chunk', 4), Item.of('alltheores:uranium_ore'))
+    compact(Item.of('exdeorum:zinc_ore_chunk', 4), Item.of('alltheores:zinc_ore'))
+    compact(Item.of('exdeorum:platinum_ore_chunk', 4), Item.of('alltheores:platinum_ore'))
+    compact(Item.of('exdeorum:osmium_ore_chunk', 4), Item.of('alltheores:osmium_ore'))
 })
 
 // This File has been authored by AllTheMods Staff, or a Community contributor for use in AllTheMods - AllTheMods 9: To the Sky.
